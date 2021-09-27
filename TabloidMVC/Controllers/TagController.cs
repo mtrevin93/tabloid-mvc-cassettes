3f68b83784ee5ecfa0a8dd6a517d826c971e4d65
Sue Xiong <sxiong0519@gmail.com>
2021-09-27 15:07:55 -0500

    created view all tags method/view

---
+++ TabloidMVC/Controllers/TagController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Repositories;
+
+namespace TabloidMVC.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ITagRepository _tagRepo;
+
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepo = tagRepository;
+        }
+        public IActionResult Index()
+        {
+            var tags = _tagRepo.GetAllTags();
+            return View(tags);
+        }
+    }
+}
---
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TabloidMVC.Models;
+using Microsoft.Data.SqlClient;
+
+namespace TabloidMVC.Repositories
+{
+    public class TagRepository : BaseRepository, ITagRepository
+    {
+        public TagRepository(IConfiguration config) : base(config) { }
+
+        public List<Tag> GetAllTags()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Tag";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Tag> tags = new List<Tag>();
+
+                    while (reader.Read())
+                    {
+                        Tag tag = new Tag
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        };
+
+                        tags.Add(tag);
+                    }
+                    reader.Close();
+                    return tags;
+                }
+                
+            }
+        }
+    }
+}
