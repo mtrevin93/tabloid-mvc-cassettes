4258bc2df8a908fca337f49031b361d3d95d51a4
Michael Trevino <mtrevin93@gmail.com>
2021-09-29 15:00:32 -0400

    reaction getall to reaction repository

--- TabloidMVC/Repositories/ReactionRepository.cs
+++ TabloidMVC/Repositories/ReactionRepository.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using TabloidMVC.Models;
+using Microsoft.Data.SqlClient;
 
 namespace TabloidMVC.Repositories
 {
-    public class ReactionRepository
+    public class ReactionRepository : BaseRepository, IReactionRepository
     {
+        public ReactionRepository(IConfiguration config) : base(config) { }
+        public List<Reaction> Get()
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Name, ImageLocation, Id
+                        FROM Reaction
+                        ";
+                    var reader = cmd.ExecuteReader();
+
+                    var reactions = new List<Reaction>();
+
+                    while (reader.Read())
+                    {
+                        reactions.Add(new Reaction()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            ImageLocation = reader.GetString(reader.GetOrdinal("ImageLocation"))
+                        });
+                    }
+
+                    reader.Close();
+
+                    return reactions;
+                }
+            }
+        }
     }
 }
